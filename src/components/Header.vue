bb8f00d60eaa18c6b92e6bf0fce2c1f2f4daed1a
douglasolv <douglas4030@gmail.com>
2021-08-10 23:35:42 -0300

    feat: add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <nav>
+      <a href="/">
+        <img
+          alt="WDEV"
+          id="logo"
+          src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg
+"
+      /></a>
+      <img
+        alt="Abrir menu"
+        id="menu-button"
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg
+"
+      />
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-background-nav);
+  width: 100%;
+  heighy: 50px;
+}
+nav {
+  display: flex;
+  justify-contnt: space-between;
+}
+
+#logo {
+  width: 130px;
+}
+
+#menu-button {
+  width: 30px;
+}
+
+.container {
+  width: 30%;
+}
+</style>
